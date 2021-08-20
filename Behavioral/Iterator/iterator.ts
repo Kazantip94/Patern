d502eee1215002ebd2c0d1bfdc7f14fff1717303
Serhii <blackdato@gmail.com>
2021-08-20 22:27:09 +0300

    fix iterator paterns

--- Behavioral/Iterator/iterator.ts
+++ Behavioral/Iterator/iterator.ts
@@ -1,48 +1,98 @@
-class myIterator {
-    private collection: Array<String | Number> = []
-    pos: number
+interface IIterator<T> {
+    current(): T
+    key(): number
+    next (): T
+    rewind(): void
+    valid(): boolean
+}
 
-    constructor(collection: Array<String | Number>) {
+interface Aggregator {
+    getIterator(): IIterator<string>
+}
+
+class myIterator implements IIterator<string> {
+    private collection: WordsCollection
+    private position: number = 0
+    private reverse: boolean = false
+
+    constructor(collection: WordsCollection, reverse: boolean = false) {
         this.collection = collection
-        this.pos = 0
-    }
+        this.reverse = reverse
 
-    next() {
-        let done = this.pos >= this.collection.length
-        if (!done) {
-            const curPos = this.pos
-            this.pos++
-            return {
-                value: this.collection[curPos],
-                done
-            }
-        } else {
-            return {
-                value: null,
-                done
-            }
+        if(reverse) {
+            this.position = collection.getCount() -1
         }
     }
 
+    next(): string {
+        const item = this.collection.getItems()[this.position]
+        this.position += this.reverse ? -1 : 1
+        return item
+    }
+
     rewind() {
-        this.pos = 0
+        this.position = this.reverse ?
+        this.collection.getCount() - 1 : 0
+    }
+
+    valid(): boolean {
+        if (this.reverse) {
+            return this.position >= 0
+        }
+        return this.position < this.collection.getCount()
+    }
+    
+    current(): string {
+        return this.collection.getItems()[this.position];
+    }
+
+    key(): number {
+        return this.position;
     }
 
 }
 
-let collection = [7, 'bravo', 'jons', 'Hello', 234, 0.3]
+class WordsCollection implements Aggregator {
+    private items: string[] = []
 
-let newIterator = new myIterator(collection)
+    getItems(): string[] {
+        return this.items
+    }
 
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.rewind())
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.next())
-console.log(newIterator.rewind())
-console.log(newIterator.next())
+    getCount(): number {
+        return this.items.length
+    }
+
+    addItem(item: string): void {
+        this.items.push(item)
+    }
+
+    getIterator(): IIterator<string> {
+        return new myIterator(this)
+    }
+
+    getReverseIterator(): IIterator<string> {
+        return new myIterator(this, true)
+    }
+}
+
+    
+const collection = new WordsCollection()
+collection.addItem('First')
+collection.addItem('Second')
+collection.addItem('Third')
+
+const iterator = collection.getIterator()
+
+console.log('Straight traversal:')
+while (iterator.valid()) {
+    console.log(iterator.next())
+}
+
+console.log('')
+console.log('Reverse traversal:')
+const reverseIterator = collection.getReverseIterator()
+while (reverseIterator.valid()) {
+    console.log(reverseIterator.next())
+}
+
