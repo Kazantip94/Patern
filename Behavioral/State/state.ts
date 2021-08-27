d71966841b3bac21980187c12709e276ead76b49
Serhii <blackdato@gmail.com>
2021-08-27 19:52:52 +0300

    fix state

--- Behavioral/State/state.ts
+++ Behavioral/State/state.ts
@@ -1,74 +1,41 @@
-enum ExampleState {
-    Initializing = 'Initializing',
-    Started = 'Started',
-    Running = 'Running',
-    Finished = 'Finished',
-}
-    
-interface IExampleState {
-    request(): void
+interface Color {
+	write(word: string): void
 }
 
-class StateContext implements IExampleState{
-    #state: ExampleState
-
-    constructor() {
-        this.#state = ExampleState.Initializing
-    }
-
-    public get state() {
-        return this.#state
-    }
-
-    public set state(value: ExampleState) {
-        switch (value) {
-            case ExampleState.Started:
-                this.request = Started.prototype.request
-                break
-            case ExampleState.Running:
-                this.request = Running.prototype.request
-                break
-            case ExampleState.Finished:
-                this.request = Finished.prototype.request
-                break
-        }
-        this.#state = value
-    }
-
-    request() {
-    }
+class Red implements Color {
+	write(word: string): void {
+		console.log(`Red color: ${word}`)
+	}
 }
 
-class Started implements IExampleState {
-    request() {
-        console.log(`I am now Started`)
-    }
+class Green implements Color {
+	write(word: string): void {
+		console.log(`Green color: ${word}`)
+	}
 }
 
-class Running implements IExampleState {
-    request() {
-        console.log(`I am now Running`)
-    }
+class TextEditor {
+	protected color: Color
+
+	constructor(color: Color) {
+		this.color = color
+	}
+
+	setColor(color: Color): void {
+		this.color = color
+	}
+
+	type(words: string): void {
+		this.color.write(words)
+	}
 }
 
-class Finished implements IExampleState {
-    request() {
-        console.log(`I am now Finished`)
-    }
-}
 
-// The Client
-const STATE_CONTEXT = new StateContext()
-console.log('STATE_CONTEXT = ' + STATE_CONTEXT.state)
-STATE_CONTEXT.state = ExampleState.Started
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Running
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Finished
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Started
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Running
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Finished
-STATE_CONTEXT.request()
+let editor = new TextEditor(new Green())
+
+editor.type('sasfsk')
+editor.type('aguh9487fh9w3478ty')
+
+editor.setColor(new Red())
+
+editor.type('23784623478')
